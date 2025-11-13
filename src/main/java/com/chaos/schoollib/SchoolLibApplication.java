18ce21f4a699b4a071914a7c2e23166b2cf6cee2
lvjianchaos <2277146577@qq.com>
2025-11-13 14:52:56 +0800

    first commit

---
+++ src/main/java/com/chaos/schoollib/SchoolLibApplication.java
@@ -0,0 +1,13 @@
+package com.chaos.schoollib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolLibApplication.class, args);
+    }
+
+}
